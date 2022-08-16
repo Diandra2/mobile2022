8e5f02e8601297a16acc59dff321eb84b756fd05
Diandra2 <diandraedwigesamaral@gmail.com>
2022-08-16 10:46:25 -0300

    16/08

--- src/screens/Camera/styles.ts
+++ src/screens/Camera/styles.ts
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     },
     textStartOver: {
         color: colors.white,
-        frontWeight: "bold",
+        fontWeight: "bold",
         textAlign: "center",
     },
     collumnPreviewVisible: {
